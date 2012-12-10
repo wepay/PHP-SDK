00c29311f6ca95f4747793716334ea19f2da2e64
Hau Nguyen <hau@Haus-MBP.local>
2012-12-10 10:59:36 -0800

    fix exceptions class for php<5.3.0

--- wepay.php
+++ wepay.php
@@ -265,7 +265,17 @@ class WePayException extends Exception {
 	public function __construct($description = '', $http_code = FALSE, $response = FALSE, $code = 0, $previous = NULL)
 	{
 		$this->response = $response;
-		parent::__construct($description, $code, $previous);
+
+		if (!defined('PHP_VERSION_ID')) {
+			$version = explode('.', PHP_VERSION);
+			define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+		}
+
+		if (PHP_VERSION_ID < 503000) {
+			parent::__construct($description, $code);
+		} else {
+			parent::__construct($description, $code, $previous);
+		}
 	}
 }
 class WePayRequestException extends WePayException {}
